338ac3d03f07d14e9492ec514694f255f4648a4d
garyniu <87505519+garyniu@users.noreply.github.com>
2022-10-28 14:24:09 -0400

    Removed and put fake 3d test onto another branch

--- ICS4U-Group-Project/MyWorld.java
+++ ICS4U-Group-Project/MyWorld.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class MyWorld here
  * 
  * @author (your name) 
  * @version (a version number or a date)
