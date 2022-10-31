29669d4cb6b0eb6db34953268d9c949b832b8318
Renokic <arshammoayedi@gmail.com>
2022-10-31 14:41:39 -0400

    some world shit ykwim
    monke

---
+++ ICS4U-Group-Project/ValueSetting.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class ValueSetting here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ValueSetting extends World
+{
+
+    /**
+     * Constructor for objects of class ValueSetting.
+     * 
+     */
+    public ValueSetting()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
