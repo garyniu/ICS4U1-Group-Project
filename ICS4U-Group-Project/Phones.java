a2e8415a8d8568e2fe8bf3cacf9a13b320b488d8
Harishan <335677076@gapps.yrdsb.ca>
2022-10-31 13:51:06 -0400

    x

---
+++ ICS4U-Group-Project/Phones.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Phones here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Phones extends Electronics
+{
+    /**
+     * Act - do whatever the Phones wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
