e46f5bf264e45d120884e25ae6af273c97109682
Renokic <arshammoayedi@gmail.com>
2022-11-03 17:38:25 -0400

    added some ints from earlier in class to item class
    mmonke

--- ICS4U-Group-Project/Items.java
+++ ICS4U-Group-Project/Items.java
@@ -12,6 +12,11 @@ public class Items extends Actor
      * Act - do whatever the Items wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    protected boolean isMade;
+    protected boolean checkPeople;
+    protected int checkLevel;
+    
     public void act()
     {
         // Add your action code here.
