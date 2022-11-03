3b48953bc869ebf0fd3010c7ed1ecc8a326156de
garyniu <87505519+garyniu@users.noreply.github.com>
2022-11-03 14:07:14 -0400

    Added conveyers

---
+++ ICS4U-Group-Project/conveyerstatic.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class conveyerstatic here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class conveyerstatic extends Machines
+{
+    /**
+     * Act - do whatever the conveyerstatic wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
