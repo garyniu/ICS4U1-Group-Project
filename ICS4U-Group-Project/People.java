f49518cc3880cd7b4eab2bfa376a1fce62081a68
garyniu <87505519+garyniu@users.noreply.github.com>
2022-11-03 14:37:37 -0400

    Converted to abstract

--- ICS4U-Group-Project/People.java
+++ ICS4U-Group-Project/People.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class People extends Actor
+public abstract class People extends Actor
 {
     protected int direction;
     protected int currentX, currentY; //current x and y positions, will be filled for inital spawning
