0b60a96bdd50c3ab16d7e91a59a9b31a7c721a75
Harishan <335677076@gapps.yrdsb.ca>
2022-11-05 19:12:21 -0400

    x

--- ICS4U-Group-Project/WorkerTwo.java
+++ ICS4U-Group-Project/WorkerTwo.java
@@ -12,6 +12,9 @@ public class WorkerTwo extends People
      * Act - do whatever the WorkerTwo wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public WorkerTwo(int locX, int locY){
+        super(locX, locY);
+    }
     public void act()
     {
         // Add your action code here.
