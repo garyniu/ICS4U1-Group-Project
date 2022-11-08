55dc7cbb95e7d2340cad64fa4ec54b95c9b6a49e
Harishan <335677076@gapps.yrdsb.ca>
2022-11-08 14:21:28 -0500

    added machine  upgrade timer

--- ICS4U-Group-Project/WorkerTwo.java
+++ ICS4U-Group-Project/WorkerTwo.java
@@ -12,6 +12,9 @@ public class WorkerTwo extends People
      * Act - do whatever the WorkerTwo wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    public WorkerTwo(int x, int y){
+        super(x, y);
+    }
     public void act()
     {
         // Add your action code here.
