868a67335478caa78ca609057898a213a8c673c0
Renokic <arshammoayedi@gmail.com>
2022-11-08 13:49:13 -0500

    Revert "Merge branch 'main' of https://github.com/garyniu/ICS4U1-Group-Project"
    This reverts commit 5d60d57a698b570e345b74a5bfa9cfd6ef887ad8, reversing
    changes made to 22edc9a0a5a0e67289c677bb56fea12225872b12.

--- ICS4U-Group-Project/ConveyorAdd.java
+++ ICS4U-Group-Project/ConveyorAdd.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class WorkConveyors here.
+ * Write a description of class ConveyorAdd here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class WorkConveyors extends Machines
+public class ConveyorAdd extends Machines
 {
     /**
-     * Act - do whatever the WorkConveyors wants to do. This method is called whenever
+     * Act - do whatever the ConveyorAdd wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
